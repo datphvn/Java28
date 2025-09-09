9db6ad327abf0187100c0e91151091256bef01e2
datphvn <ecmapdit@gmail.com>
2025-09-09 20:48:31 +0700

    Add mang danh dau bai 1

---
+++ Mang1Chieu/src/MangDanhDau1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MangDanhDau1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] cnt = new int[1000001]; //10^6 + 1 : 0 => 10^6 ban dau co gia tri tat ca la gia tri 0
+
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+        for(int i = 0; i< n; i++){
+            a[i] = sc.nextInt();
+        }
+        // 3 7 8 1 2 3 1 3
+        //cnt[3] = 1
+        for(int x : a){
+            cnt[x] = 1;
+        }
+        int dem = 0;
+        for(int val = 0; val <= 1000000; val++){
+            if(cnt[val] == 1){
+                ++dem;
+            }
+        }
+        System.out.println(dem);
+    }
+}
