1bb391df09c569b8f615340b45dc369b7c3bc2ff
datphvn <ecmapdit@gmail.com>
2025-09-18 16:25:05 +0700

    Arraylist update

--- Collection/src/Main.java
+++ Collection/src/Main.java
@@ -12,17 +12,15 @@ public class Main {
         list.add(2);
         list.add(4);
         list.add(5);
-        System.out.println(list.size());
-
-        for(int i = 0; i < list.size(); i++){
-            System.out.print(list.get(i) + " ");
+        System.out.println(list.contains(3));
+        System.out.println(list.contains(2));
+        list.set(3, 100);
+        for(int x : list){
+            System.out.println(x);
         }
-        list.add(3, 100);
-        System.out.println("");
-
-        for(int i = 0; i < list.size(); i++){
-            System.out.print(list.get(i) + " ");
-        }
+        System.out.println(list.indexOf(3));
+        System.out.println(list.indexOf(13));
+        list.remove((Object)5);
         for(int x : list){
             System.out.println(x + " ");
         }
