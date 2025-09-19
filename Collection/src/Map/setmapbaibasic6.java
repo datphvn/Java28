38201250a6d6253172c6b7d6293041b5543adac1
datphvn <ecmapdit@gmail.com>
2025-09-19 15:21:47 +0700

    Add bai set map bai basic 6

---
+++ Collection/src/Map/setmapbaibasic6.java
@@ -0,0 +1,21 @@
+package Map;
+
+import java.util.*;
+import java.util.Scanner;
+//set map co ban bai 6
+public class setmapbaibasic6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeMap<String, Integer> map = new TreeMap<>();
+        // gia tri cua phan tu trong mang, so lan xuat hien cua phan tu trong mang
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            map.put(s, 1);
+
+        }
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            System.out.print(entry.getKey() + " ");
+        }
+    }
+}
