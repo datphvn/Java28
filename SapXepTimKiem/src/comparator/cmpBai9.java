541b7d7332aba369594d2cbadd9de49d0789342c
datphvn <ecmapdit@gmail.com>
2025-09-21 12:25:48 +0700

    Add cmp bai 9

--- SapXepTimKiem/src/comparator/cmpBai9.java
+++ SapXepTimKiem/src/comparator/cmpBai9.java
@@ -3,7 +3,7 @@ package comparator;
 import java.util.*;
 import java.util.Scanner;
 
-public class cmpBai5 {
+public class cmpBai9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -12,21 +12,18 @@ public class cmpBai5 {
             a[i] = new ArrayList<>(); //cap phat dong cho a[i]
             a[i].add(sc.nextInt());
             a[i].add(sc.nextInt());
-
         }
         Arrays.sort(a, new Comparator<ArrayList<Integer>>() {
             @Override
             public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-                for (int i = 0; i < 3; i++) {
-                    if (o1.get(i) != o2.get(i)) {
-                        return o1.get(i) - o2.get(i);
-                    }
+                if(o1.get(1) != o2.get(1)){
+                    return o1.get(1) - o2.get(1);
                 }
-                return 0;
+                return o2.get(0) - o1.get(0);
             }
         });
         for(ArrayList<Integer> p : a) {
-            System.out.println(p.get(0) + " " + p.get(1) + " " + p.get(2));
+            System.out.println(p.get(0) + " " + p.get(1));
         }
     }
 
