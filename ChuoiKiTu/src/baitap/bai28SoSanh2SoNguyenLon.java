6daebdce4e505ca245bf24afc2ca431f473d2d81
datphvn <ecmapdit@gmail.com>
2025-09-25 18:28:03 +0700

    Add bai 28 so sanh 2 so nguyen lon

---
+++ ChuoiKiTu/src/baitap/bai28SoSanh2SoNguyenLon.java
@@ -0,0 +1,29 @@
+package baitap;
+
+import java.util.Scanner;
+
+public class bai28SoSanh2SoNguyenLon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        if(s.length() < t.length()){
+            System.out.println("28tech" );
+
+        }
+        else if(s.length() > t.length()){
+            System.out.println("29tech" );
+        }
+        else{
+            if(s.compareTo(t) < 0){
+                System.out.println("28tech" );
+            }
+            else if(s.compareTo(t) > 0){
+                System.out.println("29tech" );
+            }
+            else{
+                System.out.println("30tech" );
+            }
+        }
+    }
+}
