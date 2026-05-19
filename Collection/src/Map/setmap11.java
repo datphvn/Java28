742a385b4ba34eb711b5005de790e603d4b137af
datphvn <ecmapdit@gmail.com>
2026-05-19 10:53:16 +0700

    Add setmap bai 11

---
+++ Collection/src/Map/setmap11.java
@@ -0,0 +1,27 @@
+package Map;
+
+import java.util.*;
+
+public class setmap11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        TreeSet<Integer> tree1 = new TreeSet<>();
+        TreeSet<Integer> tree2 = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            tree1.add(sc.nextInt());
+
+        }
+        for (int i = 0; i < m; i++) {
+            tree2.add(sc.nextInt());
+        }
+
+        tree1.removeAll(tree2);
+        for (Integer integer : tree1) {
+            System.out.println(integer + " ");
+        }
+    }
+}
